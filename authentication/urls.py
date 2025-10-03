aca001a68ab1191c2838b069a1895301959faa3d
Fernando Lange <pymozambique@gmail.com>
2025-10-03 20:29:34 +0200

    Alter url for login

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-  path('login/', views.login_view, name='login'),
+  path('', views.login_view, name='login'),
   path('logout/', views.logout_view, name='logout'),
 ]
