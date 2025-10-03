4edcfcc17874fadc56637b060a4985121f281cc4
Fernando Lange <pymozambique@gmail.com>
2025-10-03 19:11:15 +0200

    Add permission_required and some changes on html

--- parents/views.py
+++ parents/views.py
@@ -1,20 +1,22 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ParentForm
 from .models import Parent
 
 
 @login_required(login_url='login')
-def parents_view(request):
-  parents = Parent.objects.all()
-  nav_parents = Parent.objects.all()[:4]
-  context = {'parents': parents,
-             'nav_parents': nav_parents,
-             
-  }
-  return render(request, 'parents.html', context)
+def parents_view(request):  
+    parents = Parent.objects.all()
+    nav_parents = Parent.objects.all()[:4]
+    context = {
+        'parents': parents,
+        'nav_parents': nav_parents,
+    }
+    return render(request, 'parents.html', context)
+
 
 @login_required(login_url='login')
+@permission_required('parents.add_parent', raise_exception=True)
 def parent_create(request):
     if request.method == 'POST':
         form = ParentForm(request.POST, request.FILES)
@@ -27,6 +29,7 @@ def parent_create(request):
 
 
 @login_required(login_url='login')
+@permission_required('parents.change_parent', raise_exception=True)
 def parent_update(request, pk):
     parent = get_object_or_404(Parent, pk=pk)
     
@@ -46,6 +49,7 @@ def parent_update(request, pk):
 
 
 @login_required(login_url='login')
+@permission_required('parents.delete_parent', raise_exception=True)
 def parent_delete(request, pk):
     parent = get_object_or_404(Parent, pk=pk)
     
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
 
 @login_required(login_url='login')
+@permission_required('users.add_user', raise_exception=True)
 def registration_view(request):
   if request.method == "POST":
       form = UserCreationForm(request.POST)
