7d05016058171a2911ce94f208b73f2a54f3d4c3
Fernando Lange <pymozambique@gmail.com>
2025-10-04 11:27:28 +0200

    Force cloudinary storage in model

--- parents/models.py
+++ parents/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-
+from cloudinary_backend import CloudinaryStorage
 
 
 class Parent(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
   name = models.CharField(max_length=200)
   last_name = models.CharField(max_length=200)
-  photo = models.ImageField(null=True, blank=True)
+  photo = models.ImageField(
+        upload_to='parents/', 
+        blank=True, 
+        null=True,
+        storage=CloudinaryStorage())
   birthday = models.DateField()
   slug = models.SlugField(unique=True, blank=True)
   posted_at = models.DateTimeField(auto_now_add=True)
